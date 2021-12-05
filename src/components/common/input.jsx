9925f436d2a77d21be24b3de0eaaf64775cd25b3
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-05 12:38:56 +0500

    fixed login error messages

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,4 +1,4 @@
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, onChange, error }) => {
   return (
     <div className="form-group mb-2">
       <label htmlFor={name}>{label}</label>
@@ -11,6 +11,11 @@ const Input = ({ name, label, value, onChange }) => {
         onChange={onChange}
         className="form-control"
       />
+      {error[name] && (
+        <div class="alert alert-danger" role="alert">
+          {name} is required
+        </div>
+      )}
     </div>
   );
 };
