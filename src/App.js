f08132c404bcac9b2d30fbab87d3fafb9767d705
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-11 21:43:12 +0500

    getting current user

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Movies from "./components/movies";
 import { Routes, Route, Navigate } from "react-router-dom";
 import jwtDecode from "jwt-decode";
-
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NavBar from "./components/common/navBar";
@@ -14,56 +13,37 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import React from "react";
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="container">
-//         <div className="row mb-4 ">
-//           <ToastContainer />
+class App extends React.Component {
+  state = {};
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (error) {}
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row mb-4 ">
+          <ToastContainer />
 
-//           <NavBar />
-//         </div>
-//         <Routes>
-//           <Route path="/" element={<Navigate replace to="/movies" />} />
-//           <Route path="/movies" element={<Movies />} />
-//           <Route path="/movies/:id" element={<MoviesForm />} />
-//           <Route path="/customers" element={<Customers />} />
-//           <Route path="/rentals" element={<Rentals />} />
-//           <Route path="/login" element={<LoginForm />} />
-//           <Route path="/register" element={<RegisterForm />} />
-//           <Route path="/not-found" element={<NotFound />} />
-//           <Route path="*" element={<Navigate replace to="/not-found" />} />
-//         </Routes>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-function App() {
-  // const state = {};
-
-  return (
-    <div className="container">
-      <div className="row mb-4 ">
-        <ToastContainer />
-
-        <NavBar />
+          <NavBar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/movies" />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MoviesForm />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/rentals" element={<Rentals />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate replace to="/not-found" />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/movies" />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MoviesForm />} />
-        <Route path="/customers" element={<Customers />} />
-        <Route path="/rentals" element={<Rentals />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/not-found" element={<NotFound />} />
-        <Route path="*" element={<Navigate replace to="/not-found" />} />
-      </Routes>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
