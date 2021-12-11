fe08ff03f7946a64acdaa96c596217afc4ef93fb
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-11 18:29:35 +0500

    backend for login

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "./../services/authService";
+import { toast } from "react-toastify";
 
 class LoginForm extends Form {
   state = {
@@ -13,9 +15,20 @@ class LoginForm extends Form {
     password: Joi.string().required().label("Password"),
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     //Server calling
-    console.log("form submitted");
+    const { data } = this.state;
+
+    try {
+      await login(data.username, data.password);
+      toast.success("User added sucessfuly!");
+      console.log("login done");
+    } catch (error) {
+      const errors = { ...this.state.errors };
+      errors.username = error.response.data;
+      this.setState({ errors });
+      toast.error("Something went wrong");
+    }
   };
 
   render() {
