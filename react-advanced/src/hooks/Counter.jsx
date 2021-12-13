deae65d844f2eab46c706865040d0de26b152960
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-13 11:03:39 +0500

    useState hook

---
+++ react-advanced/src/hooks/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment, useState } from "react";
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  return (
+    <Fragment>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <div>
+        {name} has changed counter {count} times!{" "}
+      </div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase{" "}
+      </button>
+    </Fragment>
+  );
+}
+
+export default Counter;
