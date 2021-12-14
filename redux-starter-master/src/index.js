59424d8cc59c39c6e5a1f2fa6e0d1b4eac4dd196
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-14 12:35:52 +0500

    cleaned up code to start fresh

--- redux-starter-master/src/index.js
+++ redux-starter-master/src/index.js
@@ -1,12 +1,1 @@
-import store from "./store";
-import { bugAdded, bugResolved } from "./actions";
-
-store.subscribe(() => {
-	console.log("Store changed!", store.getState());
-});
-
-store.dispatch(bugAdded("Bug 1"));
-
-store.dispatch(bugResolved(1));
-
-console.log(store.getState());
+console.log("hello redux");
